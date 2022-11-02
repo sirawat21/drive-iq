de7f5c3e631d178ee295e9cc04ec7bbba44180d8
Brett Sira <brett@Bretts-MacBook-Pro.local>
2022-11-03 08:27:44 +1100

    create sql source files

---
+++ src/create.db.sql
@@ -0,0 +1,36 @@
+-- ###########################################################################
+--                  CREATE DATABASE AND TABLES
+-- ###########################################################################
+
+-- ---------------------------------------------------------------------------
+-- INITIALISE DATABBASE WITH CHARACTER SET AS UTF-8 UNICODE COLLATION
+-- ---------------------------------------------------------------------------
+CREATE DATABASE IF NOT EXISTS databse
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+USE demoDB;
+SHOW TABLES;
+
+-- ---------------------------------------------------------------------------
+--  CUSTOMER TABLE
+-- ---------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS CUSTOMER (
+    ID          INT             PRIMARY KEY AUTO_INCREMENT,
+    NAME        VARCHAR(30),
+    AMOUNT      INT(255),
+    BROKER_ID   INT
+) ENGINE = InnoDB;
+
+-- ---------------------------------------------------------------------------
+--  BROKER TABLE
+-- ---------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS BROKER (
+    ID          INT             PRIMARY KEY,
+    NAME        VARCHAR(30)
+) ENGINE = InnoDB;
+
+-- ---------------------------------------------------------------------------
+-- ALTER TABLE FOR FOREIGN KEYS
+-- ---------------------------------------------------------------------------
+ALTER TABLE CUSTOMER
+ADD FOREIGN KEY (BROKER_ID) REFERENCES BROKER(ID);
